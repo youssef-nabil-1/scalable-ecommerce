29832e865df926a969e38985809e543be40765fe
Youssef Nabil <youssef.in6@gmail.com>
2025-08-14 05:07:11 +0300

    update auth routes

--- user-service/app.js
+++ user-service/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
 
 const userRoutes = require("./routes/user");
 
 const app = express();
 
-app.use(userRoutes);
+app.use(bodyparser.json());
+
+app.use("/auth", userRoutes);
 
 mongoose
     .connect(process.env.MONGO_URI)
