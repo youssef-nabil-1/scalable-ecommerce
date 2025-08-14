bbf906231ed21e57c949e6e497be635c7d9647c0
Youssef Nabil <youssef.in6@gmail.com>
2025-08-14 04:54:41 +0300

    add authentication service

--- user-service/controllers/user.js
+++ user-service/controllers/user.js
@@ -15,3 +15,23 @@ exports.createUser = async (req, res, next) => {
         user: result._id,
     });
 };
+
+exports.login = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({ email: email });
+    if (!user) {
+        return res.status(404).json({ message: "Email not found" });
+    }
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+        return res.status(401).json({ message: "Wrong email or password" });
+    }
+    const token = jwt.sign(
+        { userId: user.id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+    );
+    res.status(200).json({ message: "Logged in successfully", token });
+};
